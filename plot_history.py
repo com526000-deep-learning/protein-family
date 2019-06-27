9dc16f1bb3545dcb6827225fc750f6686eb0ba6d
yuchiatw <claire.hsinchu@gmail.com>
2019-06-28 04:20:48 +0800

    change order

--- plot_history.py
+++ plot_history.py
@@ -7,28 +7,24 @@ def plotFigure(df, name):
     acc_ts = df[1]
     plt.plot(np.arange(len(acc_ts)), acc_ts, label = name)    
 
-
-
 fname = "1DCNN_10"
 df = pickle.load(open('{}.pickle'.format(fname), 'rb'))
-plotFigure(df, "1D-CNN, top 10")
+plotFigure(df, "1D-CNN")
 
 fname = "1DCNN_10_undersample"
 df = pickle.load(open('{}.pickle'.format(fname), 'rb'))
-plotFigure(df, "1D-CNN, top 10, 4-gram")
+plotFigure(df, "1D-CNN, 4-gram")
 
 fname = "GRU_ngram_10_undersample"
 df = pickle.load(open('{}.pickle'.format(fname), 'rb'))
-plotFigure(df, "GRU, top 10, 3-gram")
+plotFigure(df, "GRU, 3-gram")
 
 fname = "cnn_gru_ngram_10"
 df = pickle.load(open('{}.pickle'.format(fname), 'rb'))
-plotFigure(df, "CNN + GRU, top 10, 3-gram")
+plotFigure(df, "CNN + GRU, 3-gram")
 
 plt.legend()
 plt.xlabel("epoch")
 plt.ylabel("acc")
-plt.title("learning curve")
+plt.title("learning curve (top 10)")
 plt.savefig("learning_curve")
-
-
